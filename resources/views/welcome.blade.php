f05b618d526a871a61f084b8f01b43a2a5fd304a
Your Name <you@example.com>
2017-11-15 15:04:08 -0300

    Hola mundo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hola mundo
                 </div>
 
                 <div class="links">
